5253e1c6d62173ec0b19a095f0c3973cd3865f50
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-14 07:54:46 -0300

    feat: add environment variable imports

---
+++ app/settings.py
@@ -0,0 +1,10 @@
+from os import getenv
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+RABBITMQ_DEFAULT_USER = getenv("RABBITMQ_DEFAULT_USER") or "user"
+RABBITMQ_DEFAULT_PASS = getenv("RABBITMQ_DEFAULT_PASS") or "password"
+RABBITMQ_DEFAULT_HOST = getenv("RABBITMQ_DEFAULT_HOST") or "localhost"
+RABBITMQ_MAIN_QUEUE = getenv("RABBITMQ_MAIN_QUEUE") or "hello"
