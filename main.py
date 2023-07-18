760b2d2ff90de7c4ac7b9772a45ad7ee7ffcb2b8
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-18 14:49:37 -0300

    feat: update main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from app.publisher.publisher import RabbitMQPublisher
 from json import dumps
 
+l = ['B07H6DPQ6B', 'B07N1G1THD', 'B09VZ8YC6G', 'B09VCPNY1T', 'B096QY7ZQQ', 'B09WVLVWGY', 'B09NQ8WQ5Q', 'B095HM612T', 'B09C22CQQV', 'B09VP8S3PN', 'B0BZQNPM6D', 'B09X3492XJ', 'B09BZNTVQC', 'B0C11HL98H', 'B08TTLMKXG', 'B0BZ16FM6X']
+
 if __name__ == "__main__":
   try:
     publisher = RabbitMQPublisher()
-    publisher.publish_message(dumps([i for i in range(10)]))
+    publisher.publish_message(dumps(l))
   except KeyboardInterrupt:
     print('Interrupted')
