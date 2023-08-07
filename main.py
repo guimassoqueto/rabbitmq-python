a43022cd65b7b8b83ceef4a7be0bce4999aeba7a
guimassoqueto <gui.massoqueto@gmail.com>
2023-08-07 14:13:33 -0300

    feat: update main

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 from app.publisher.publisher import RabbitMQPublisher
+from app.csv.ids_to_list import ids_to_list
+from app.delete_file.delete_file import delete_file
 from json import dumps
+from sys import argv
 
-l = ['B07H6DPQ6B', 'B07N1G1THD', 'B09VZ8YC6G', 'B09VCPNY1T', 'B096QY7ZQQ', 'B09WVLVWGY', 'B09NQ8WQ5Q', 'B095HM612T', 'B09C22CQQV', 'B09VP8S3PN', 'B0BZQNPM6D', 'B09X3492XJ', 'B09BZNTVQC', 'B0C11HL98H', 'B08TTLMKXG', 'B0BZ16FM6X']
+def template_for() -> int:
+  template_for = input("[T]THUNDER [K]KADEC: ")
+  while True:
+    template_for = template_for.lower()
+    if template_for == 't': return 1
+    if template_for == 'k': return 2
+    
+    print('INVALID OPTION. THE OPTIONS ARE T OR K.\n')
+    template_for = input("[T]THUNDER [K]KADEC: ")
+
 
 if __name__ == "__main__":
+  filename = 'data.csv'
   try:
+    option = template_for()
+
+    if option == 1: 
+      l = {"thunder": ids_to_list(filename)}
+    else:
+      l = {"kadec": ids_to_list(filename)}
+
+    print(l)
+
     publisher = RabbitMQPublisher()
     publisher.publish_message(dumps(l))
-  except KeyboardInterrupt:
-    print('Interrupted')
+    delete_file(filename)
+  except Exception as e:
+    print(e)
