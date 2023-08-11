09425ec904ca271edc3ce72044c84e9b805f0dc2
guimassoqueto <gui.massoqueto@gmail.com>
2023-08-11 17:12:23 -0300

    feat: update message structure

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ from app.delete_file.delete_file import delete_file
 from json import dumps
 from sys import argv
 
-def template_for() -> int:
+def template_for() -> str:
   template_for = input("[T]THUNDER [K]KADEC: ")
   while True:
     template_for = template_for.lower()
-    if template_for == 't': return 1
-    if template_for == 'k': return 2
+
+    if template_for == 't':
+      thunder_options = input("[S]STORY [FF]FEED FULL [FQ]FEED QUARTER: ").lower()
+      if thunder_options == 's': return 'ts'
+      if thunder_options == 'ff': return 'tff'
+      if thunder_options == 'fq': return 'tfq'
+
+    if template_for == 'k': return 'k'
     
     print('INVALID OPTION. THE OPTIONS ARE T OR K.\n')
     template_for = input("[T]THUNDER [K]KADEC: ")
@@ -19,13 +25,7 @@ if __name__ == "__main__":
   filename = 'data.csv'
   try:
     option = template_for()
-
-    if option == 1: 
-      l = {"thunder": ids_to_list(filename)}
-    else:
-      l = {"kadec": ids_to_list(filename)}
-
-    print(l)
+    l = { option: ids_to_list(filename) }
 
     publisher = RabbitMQPublisher()
     publisher.publish_message(dumps(l))
